6468febf4908144b21ec710537045eace67d554c
Kayanr <kayan.reid@gmail.com>
2020-04-10 18:54:19 -0400

    Updated index.js
    Updated index.js

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+let header = <h1>JAVA</h1>
+let text = <p> <h2>Certificate Program</h2>
+         This comprehensive course introduces software engineering using the JAVA programming language with object-oriented programming principles. Learn primitive and non-primitive data types, control flow constructs, built-in class libraries, and event-driven programming methods. Create and manipulate objects, classes and how to use object-oriented tools such as the class debugger. Learn how to design, create, build and debug Java applications and applets. Apply algorithmic thinking to solve programming problems. Implement syntax rules in Java programs and explain variables and data types used in program development and more. This course is designed for students with some programming experience and/or have some familiarity with Object Oriented Programming.</p>
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    {header}
+    {text}
+  </div>,
   document.getElementById('root')
 );
 
