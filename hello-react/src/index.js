6a0a85b0aacc9957c8e5a41d7f2baf8fe6c48abf
Kayanr <kayan.reid@gmail.com>
2020-04-15 20:37:24 -0400

    added the last ordered list
    added the last ordered list

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -7,6 +7,12 @@ import * as serviceWorker from './serviceWorker';
 let header = <h1>JAVA</h1>
 let subheader = <h2>Certificate Program</h2>
 let text = <p>This comprehensive course introduces software engineering using the JAVA programming language with object-oriented programming principles. Learn primitive and non-primitive data types, control flow constructs, built-in class libraries, and event-driven programming methods. Create and manipulate objects, classes and how to use object-oriented tools such as the class debugger. Learn how to design, create, build and debug Java applications and applets. Apply algorithmic thinking to solve programming problems. Implement syntax rules in Java programs and explain variables and data types used in program development and more. This course is designed for students with some programming experience and/or have some familiarity with Object Oriented Programming. </p>
+ let lastList = <ol>
+ <li>Complete online <u>JAVA application</u></li>
+ <li>Attend one <u >mandatory information session.</u></li>
+ <li>Take assessment test</li>
+ <li>Be selected for enrollment</li>
+ </ol>
 let applyButton = <div>
 <button>APPLY FOR THIS PROGRAM </button>
 </div>
@@ -33,6 +39,7 @@ ReactDOM.render(
     {header}
     {subheader}
     {text}
+    {lastList}
     {applyButton}
   </div>,
   document.getElementById('root')
