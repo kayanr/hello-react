a2b1845f7d23cbf066f938e82444cd7662c8c6da
Kayanr <kayan.reid@gmail.com>
2020-04-15 20:01:15 -0400

    changes made to index.js
    modifying the index.js to look like assignment

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -5,11 +5,30 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 let header = <h1>JAVA</h1>
-let text = <p> <h2>Certificate Program</h2>
-         This comprehensive course introduces software engineering using the JAVA programming language with object-oriented programming principles. Learn primitive and non-primitive data types, control flow constructs, built-in class libraries, and event-driven programming methods. Create and manipulate objects, classes and how to use object-oriented tools such as the class debugger. Learn how to design, create, build and debug Java applications and applets. Apply algorithmic thinking to solve programming problems. Implement syntax rules in Java programs and explain variables and data types used in program development and more. This course is designed for students with some programming experience and/or have some familiarity with Object Oriented Programming.</p>
+let subheader = <h2>Certificate Program</h2>
+let text = <p>This comprehensive course introduces software engineering using the JAVA programming language with object-oriented programming principles. Learn primitive and non-primitive data types, control flow constructs, built-in class libraries, and event-driven programming methods. Create and manipulate objects, classes and how to use object-oriented tools such as the class debugger. Learn how to design, create, build and debug Java applications and applets. Apply algorithmic thinking to solve programming problems. Implement syntax rules in Java programs and explain variables and data types used in program development and more. This course is designed for students with some programming experience and/or have some familiarity with Object Oriented Programming. </p>
+
+/* Applications are still being accepted for this(FREE) scholarship funded course. Don't miss this opportunity to advance your career!
+This program is designed for:</p>);
+let list = (
+<ul>
+    <li> students with some programming experience and/or those who work at a banking or 
+    technology company and are seeking upward mobility.</li>
+</ul>
+<b>Application process:</b>
+
+<ol>
+<li>Complete online <u style ="color:blue">JAVA application</u></li>
+<li>Attend one <u style ="color:blue">mandatory information session.</u></li>
+<li>Take assessment test</li>
+<li>Be selected for enrollment</li>
+</ol>  */
+
+
 ReactDOM.render(
   <div>
     {header}
+    {subheader}
     {text}
   </div>,
   document.getElementById('root')
